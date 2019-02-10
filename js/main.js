7b52cb2acbea305ad5ccbc4450f479a2c023df67
leotalorac <leotalorac@unal.edu.co>
2019-02-10 12:41:05 -0500

    save the data on a datastructure

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-/*
-    Feel free to put your custom js here.
-  */
+const linkpoligons = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson"
+const nottoshow = [58,29,65,64,20,66,67,9,39,14]
 //scroll down
 $("#tablebutton").click(function() {
     $('.infodisplay').animate({
@@ -13,3 +12,17 @@ $("#mapbutton").click(function() {
         scrollTop: $(".mapcontainer").offset().top},
         "slow");
 });
+
+var poligons
+function getDatageo(url){
+	let datafull;
+	let data = $.get(url,()=>{})
+		.done(() => {
+		    poligons = JSON.parse(data.responseText).features;
+		});
+	return new Promise(resolve => {
+        setTimeout(()=>{
+            resolve("yessd");
+        },1000);
+	});
+}
--- js/map.js
+++ js/map.js
@@ -1,17 +1,42 @@
 //map.js create by: Luis Eduardo Otalora Cubides
 var map;
+var upos = {lat: 40.729364,lng: -73.996480};
+var colors =["red","blue","green","black","yellow"]
+var districts ={}
 //put the poligons on the map
-function getpoligons() {
-  map.data.loadGeoJson('https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson');
-  map.data.setStyle({
-    fillColor: '#b097c1',
-    strokeColor: "#b097c1"
+//draw the poligons
+async function drawpoligons() {
+  map.data.loadGeoJson(linkpoligons);
+  await getDatageo(linkpoligons);
+  map.data.setStyle(function (feature){  
+    let id = feature.l.OBJECTID;
+    let boor = parseInt(String(feature.l.BoroCD)[0]);
+    let show = true;
+    if(nottoshow.includes(id)){
+      show = false;
+    }else{
+      districts[id] = {
+        "nb":boor,
+        "form":poligons[id-1].geometry.coordinates[0]
+      }
+    }
+    return({
+      title:id,
+      fillColor: "#b097c1",
+      strokeColor: "#b097c1",
+      visible:show
+    })
+  });  
+}
+function putmarker(position,icon,title){
+  var marker = new google.maps.Marker({
+    position:position,
+    map: map,
+    icon: icon,
+    title:title,
   });
 }
-//draw the poligons
-function drawpoligons() {
-  getpoligons();
-}
+
 //map response
 function onGoogleMapResponse() {
   map = new google.maps.Map(document.getElementById('googleMapContainer'), {
@@ -22,16 +47,6 @@ function onGoogleMapResponse() {
     zoom: 11
   });
   //put the university marker
-  var marker = new google.maps.Marker({
-    position: {
-      lat: 40.729364,
-      lng: -73.996480
-    },
-    map: map,
-    icon: {
-      url: "https://img.icons8.com/color/48/000000/region-code.png"
-    },
-    title: 'NYU'
-  });
+  putmarker(upos,{url: "https://img.icons8.com/color/48/000000/region-code.png"},"NYU");
   drawpoligons();
 }
