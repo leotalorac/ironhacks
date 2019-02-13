31f69623e6fa91440a608fe5e0f8719185eb71ff
lotalorafox <lotalorafox@outlook.com>
2019-02-13 10:55:54 -0500

    get the poligons and draw

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const linkpoligons = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson"
 const nottoshow = [58,29,65,64,20,66,67,9,39,14]
+var districts ={}
+var poligons ={};
+let poglo;
 //scroll down
 $("#tablebutton").click(function() {
     $('.infodisplay').animate({
@@ -12,17 +15,68 @@ $("#mapbutton").click(function() {
         scrollTop: $(".mapcontainer").offset().top},
         "slow");
 });
-
-var poligons
 function getDatageo(url){
 	let datafull;
 	let data = $.get(url,()=>{})
 		.done(() => {
-		    poligons = JSON.parse(data.responseText).features;
+            let tem = JSON.parse(data.responseText).features;
+            poglo = tem
+            console.log(tem[9])    
+            for (i in tem){
+                if(!(nottoshow.includes(tem[i]["id"]))){
+                    let coords = tem[i]["geometry"]["coordinates"][0]
+                    let jsoncoods =[]
+                    if(tem[i]["geometry"]["type"] === "Polygon"){
+                        let coords = tem[i]["geometry"]["coordinates"][0]
+                        coords.forEach((element) =>{
+                            jsoncoods.push({
+                                "lat":element[1],
+                                "lng":element[0]
+                            })
+                        })
+                    }else{
+                        let coords = tem[i]["geometry"]["coordinates"]
+                        coords.forEach((element) =>{
+                            let jarr = []
+                            element.forEach((e) =>{
+                                e.forEach((v) =>{
+                                    jarr.push({
+                                        "lat":v[1],
+                                        "lng":v[0]
+                                    })
+                                })
+                            });
+                            jsoncoods.push(jarr)
+                        })
+                    }
+                    poligons[i] = {
+                        "id": tem[i]["id"],
+                        "type":tem[i]["geometry"]["type"],
+                        "coords":jsoncoods
+                    }
+                }
+            }
 		});
 	return new Promise(resolve => {
         setTimeout(()=>{
-            resolve("yessd");
+            resolve("yess");
         },1000);
 	});
+}
+function calculateCenter(){
+    let d = Object.keys(districts);
+    for(let i =0;i<2;i++){
+        let lat = 0;
+        let lng =0;
+        let t =districts[d[i]]["form"].length;
+        //console.log(districts[d[i]]["form"])
+        for(let y =0;y<t;t++){
+            lat += districts[d[i]]["form"][y][0];
+            lng += districts[d[i]]["form"][y][1];
+        }
+        lat = lat/t;
+        lng = lng/t
+        districts[d[i]]["center"] = {lat:lat,lng:lng};
+        //console.log(districts[i])
+    }
 }
--- js/map.js
+++ js/map.js
@@ -1,42 +1,46 @@
 //map.js create by: Luis Eduardo Otalora Cubides
 var map;
-var upos = {lat: 40.729364,lng: -73.996480};
+var mappoligons=[];
+var mapmarkers=[];
+var upos = {"lat": 40.729364,"lng": -73.996480};
 var colors =["red","blue","green","black","yellow"]
-var districts ={}
+var maxdist = 0.2658503482103375
 //put the poligons on the map
 //draw the poligons
 async function drawpoligons() {
-  map.data.loadGeoJson(linkpoligons);
+  //map.data.loadGeoJson(linkpoligons);
   await getDatageo(linkpoligons);
-  map.data.setStyle(function (feature){  
-    let id = feature.l.OBJECTID;
-    let boor = parseInt(String(feature.l.BoroCD)[0]);
-    let show = true;
-    if(nottoshow.includes(id)){
-      show = false;
+  for(i in poligons){
+    if(poligons[i].type == "Polygon"){
+      putpoligon(poligons[i].coords);
     }else{
-      districts[id] = {
-        "nb":boor,
-        "form":poligons[id-1].geometry.coordinates[0]
-      }
+      poligons[i].coords.forEach((element) =>{
+        putpoligon(element);
+      })
     }
-    return({
-      title:id,
-      fillColor: "#b097c1",
-      strokeColor: "#b097c1",
-      visible:show
-    })
-  });  
+  }
+}
+function putpoligon(coords){
+  var tempol = new google.maps.Polygon({
+    paths: coords,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 3,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+  tempol.setMap(map);
+  mappoligons.push(tempol);
+
 }
 function putmarker(position,icon,title){
   var marker = new google.maps.Marker({
     position:position,
     map: map,
     icon: icon,
-    title:title,
+    title:title
   });
 }
-
 //map response
 function onGoogleMapResponse() {
   map = new google.maps.Map(document.getElementById('googleMapContainer'), {
