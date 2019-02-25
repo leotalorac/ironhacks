1a315768498b8d5406ffbf7ca8858d1b6d4cda84
leotalorac <leotalorac@unal.edu.co>
2019-02-24 20:55:22 -0500

    start to the new

--- js/map.js
+++ js/map.js
@@ -1,61 +1,4 @@
-//map.js create by: Luis Eduardo Otalora Cubides
-
-//put the poligons on the map
-//draw the poligons
-async function drawpoligons() {
-  //map.data.loadGeoJson(linkpoligons);
-  await getDatageo(linkpoligons);
-  for(i in poligons){
-    districts[i]["poligs"]=[]
-    districts[i]["center"]={}
-    districts[i]["id"]=poligons[i].id
-    if(poligons[i].type == "Polygon"){
-      putpoligon(poligons[i].coords,i);
-    }else{
-      poligons[i].coords.forEach((element) =>{
-        putpoligon(element,i);
-      })
-    }
-  }
-  await calculateDistances(); 
-  
-}
-function putpoligon(coords,i){
-  var tempol = new google.maps.Polygon({
-    paths: coords,
-    strokeColor: '#C29CFF',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#C29CFF',
-    fillOpacity: 0.35,
-    title: String(poligons[i].id)
-  });
-  tempol.setMap(map);
-  mappoligons.push(tempol);
-  tempol.addListener('click', (event) => {console.log(districts[i])});
-  districts[i]["poligs"].push(mappoligons.length-1);
-  let bounds = new google.maps.LatLngBounds();
-  coords.forEach(element => {
-    bounds.extend(element);
-  });
-  if(Object.keys(districts[i]["center"]).length === 0){
-    districts[i]["center"]={"lat":bounds.getCenter().lat(),"lng":bounds.getCenter().lng()}
-  }else{
-    let lat = (districts[i]["center"]["lat"]+bounds.getCenter().lat())/2;
-    let lng = (districts[i]["center"]["lng"]+bounds.getCenter().lng())/2; 
-    districts[i]["center"]={"lat":lat,"lng":lng}
-  }
-  
-}
-function putmarker(position,icon,title){
-  var marker = new google.maps.Marker({
-    position:position,
-    map: map,
-    icon: icon,
-    title:title    
-  });
-}
-//map response
+//------------------------map general function ----------------------------
 async function onGoogleMapResponse() {
   map = new google.maps.Map(document.getElementById('googleMapContainer'), {
     center: {
@@ -336,8 +279,66 @@ async function onGoogleMapResponse() {
 ]
   });
   //put the university marker
-  putmarker(upos,{url: "https://img.icons8.com/color/48/000000/region-code.png"},"NYU");
-  await drawpoligons();
-  getDatarisk(linkrisk);
-  getDataaff(linkaff);
+  putMarker(upos,{url: "https://img.icons8.com/color/48/000000/region-code.png"},"NYU");
+}
+
+//-----------------------------ultils functions --------------------------------------
+//put a marker in the map
+function putMarker(position,icon,title){
+    //create and set the marker's propieties
+    var marker = new google.maps.Marker({
+    position:position,
+    map: map,
+    icon: icon,
+    title:title    
+    });
+    //add it to the general array
+    mapmarkers.push(marker);
+}
+//create and put a poligon in the map
+function putPolygon(coords,parent){
+    //create the poligon
+    var tempol = new google.maps.Polygon({
+        paths: coords,
+        strokeColor: '#C29CFF',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#C29CFF',
+        fillOpacity: 0.35,
+        map
+    });
+    attachPolygonInfoWindow(tempol,"District " + parent.id)
+    //saved on the data structure
+    parent.poligs.push(tempol);
+    //set the center of te poligon and saver 
+    setCenter(coords,parent);
+}
+// save the center on the data structure
+function setCenter(coords,parent,options){
+    //calculate the center
+    let bounds = new google.maps.LatLngBounds();
+    coords.forEach(element => {
+      bounds.extend(element);
+    });
+    //saved
+    parent.center={"lat":bounds.getCenter().lat(),"lng":bounds.getCenter().lng()};
+}
+//fucntion to know is in a poligon
+function isinPolygon(point,polygon){
+    //create a latlng object
+    let coordinate = new google.maps.LatLng(point);
+    //look for it is in
+    return google.maps.geometry.poly.containsLocation(coordinate, polygon);
+}
+//----------------------------draw general functions ----------------------
+
+function drawPolygons(){
+    //go over districs
+    for(id in districts){
+        //get the coords and draw the poligons
+        districts[id].coords.forEach(bounds => {
+            //draw the poligon on the map
+            putPolygon(bounds,districts[id]);
+        });
+    }
 }
